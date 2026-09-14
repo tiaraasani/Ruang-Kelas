35a556b3468848f54beb29a75581f5389d732da3
tiara sani <nabila.mutiarasani@gmail.com>
2026-09-14 11:31:52 +0700

    chore: remove legacy vulnerable PHP application

--- aksi_logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-?>
-    <script language="javascript">
-        alert("Anda Yakin Akan Logout??");
-        document.location="form_login.php";
-    </script>
--- koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-        $host = "localhost:3306";
-        $username = "root";
-        $password = "";
-        $database = "ruangKelas";
-        $koneksi = mysqli_connect($host, $username, $password, $database);
-        ?>
